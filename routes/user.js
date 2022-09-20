191f84a958f594988c8a8dfd07676483f661cc4f
trivedi2u <shashanktrivedi.official@gmail.com>
2022-09-20 08:10:00 +0530

    ok

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const multer = require("multer");
 const userController = require('../controllers/userController');
 
-
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, './uploads');
